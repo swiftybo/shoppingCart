5fd99546815853abcfe1b5183abf7f3305fa279a
swiftybo <leungjordan28@gmail.com>
2025-11-05 20:40:54 +0000

    Completed styling for productIcon in productItem

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,11 +1,17 @@
 function ProductItem({ name, stock, price, category }) {
   return (
-    <div className="productIcon">
-      <h2>{name}</h2>
-      <div style={{ fontSize: "1.5rem" }}>${price}</div>
-      <button className="addProductBtn">Add to Cart</button>
-      {/* <div>Stock: {stock}</div> */}
-    </div>
+    <section className="productIcon">
+      <div className="productIcon-left">
+        <h2 style={{ marginTop: "0rem", marginBottom: "1.5rem" }}>{name}</h2>
+        <div style={{ fontSize: "1.2rem" }}>${price.toFixed(2)}</div>
+      </div>
+      <div className="productIcon-right">
+        <button className="productIcon_btn">Add to Cart</button>
+        {stock <= 5 && (
+          <div className="productIcon-stock">Almost Gone! Stock: {stock}</div>
+        )}
+      </div>
+    </section>
   );
 }
 
