93baa493f8618d758ad9483d3a51390fed4b1120
swiftybo <leungjordan28@gmail.com>
2025-11-05 20:13:19 +0000

    Added more styling for ProductItem elements

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,10 +1,11 @@
 function ProductItem({ name, stock, price, category }) {
   return (
-    <>
-      <h3>{name}</h3>
-      <div>Stock: {stock}</div>
-      <div>{price}</div>
-    </>
+    <div className="productIcon">
+      <h2>{name}</h2>
+      <div style={{ fontSize: "1.5rem" }}>${price}</div>
+      <button className="addProductBtn">Add to Cart</button>
+      {/* <div>Stock: {stock}</div> */}
+    </div>
   );
 }
 
