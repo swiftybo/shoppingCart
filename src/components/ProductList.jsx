b2dfa0319ff30aed44599e453c892abecc62fb48
swiftybo <leungjordan28@gmail.com>
2025-11-21 12:21:56 +0000

    Removed the reducer functionn completely from ProductList

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -17,62 +17,6 @@ const catalogue = [
   { name: "Leek", stock: 4, price: 0.9, category: "vegetable" },
 ];
 
-function reducer(state, action) {
-  switch (action.type) {
-    // Add a new object with the action.payload being the name of the product to the shopping cart state array
-    case "add_product":
-      return [...state, { productName: action.payload, quantity: 1 }];
-    case "increase_quantity":
-      state.map((product) => {
-        if (product.productName !== action.payload) {
-          return;
-        }
-
-        console.log(`product is still ${product["productName"]}`);
-
-        // Finding index of the productItem object with the name equivalent to action.payload
-        const currentProductIndex = state.findIndex(
-          (product) => product.productName === "apple"
-        );
-
-        // Saving the previous quantity of this productItem in a separate variable
-        const previousQuantity = state[currentProductIndex].quantity;
-
-        // Creating new object with updated quantity value
-        const updatedProduct = {
-          ...state[currentProductIndex],
-          quantity: previousQuantity + 1,
-        };
-
-        const newState = [...state];
-        newState[currentProductIndex] = updatedProduct;
-        console.log(newState);
-
-        return newState;
-    
-    // case "increase_quantity": {
-      // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
-    //   const newState = state.map((product) => {
-    //     if (product.productName === action.payload) {
-    //       // Updates the quantity of the desired product
-    //       return {
-    //         productName: product.productName,
-    //         quantity: product.quantity + 1,
-    //       };
-    //       // For all items in the shopping cart array, dont do any changes
-    //     } else {
-    //       return product;
-    //     }
-    //   });
-    //   // return the new state of the shopping cart with quantity increased for a specific product
-    //   return newState;
-    // }
-
-//     default:
-//       return state;
-//       }
-// }
-
 function ProductList() {
   const [state, dispatch] = useReducer(reducer, initialState);
 
