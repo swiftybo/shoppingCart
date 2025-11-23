7619eedbb4ac10e18234a2fdd98a8b99bb5ae5c0
swiftybo <leungjordan28@gmail.com>
2025-11-23 17:48:46 +0000

    Added error page to notify user when navigating to non-existing page

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,9 @@
-import HomePage from "./pages/HomePage";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
-import ShoppingCartPage from "./pages/ShoppingCartPage";
 import { FoodProvider } from "./contexts/FoodContext";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
+import HomePage from "./pages/HomePage";
+import ShoppingCartPage from "./pages/ShoppingCartPage";
+import ErrorPage from "./pages/ErrorPage";
 
 function App() {
   return (
@@ -11,6 +13,7 @@ function App() {
           <Routes>
             <Route index element={<HomePage />} />
             <Route path="cart" element={<ShoppingCartPage />} />
+            <Route path="*" element={<ErrorPage />} />
           </Routes>
         </BrowserRouter>
       </FoodProvider>
--- src/pages/ErrorPage.jsx
+++ src/pages/ErrorPage.jsx
@@ -2,12 +2,15 @@ import { Link } from "react-router-dom";
 
 export default function ErrorPage() {
   return (
-    <div>
-      Error. The page you're looking for does not exist. Please navigate back to
-      the Almighty's HomePage{" "}
-      <Link to={"/"} end>
-        here
-      </Link>
-    </div>
+    <>
+      <h1 style={{ color: "red", textAlign: "center" }}>Error!</h1>
+      <div style={{ textAlign: "center" }}>
+        The page you're looking for does not exist. Please navigate back to the
+        Almighty's HomePage{" "}
+        <Link to={"/"} end>
+          here
+        </Link>
+      </div>
+    </>
   );
 }
