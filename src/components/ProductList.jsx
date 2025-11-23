85a9520a5cf011e8ee0b9562e865919adfe1eabc
swiftybo <leungjordan28@gmail.com>
2025-11-23 16:12:23 +0000

    Moved reduced to context

--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,10 +1,6 @@
 import ProductItem from "./ProductItem.jsx";
 import { useReducer } from "react";
 
-const initialState = [
-  { productName: "Leek", quantity: 2 },
-  { productName: "Broccoli", quantity: 5 },
-];
 const catalogue = [
   {
     name: "Broccoli",
@@ -17,46 +13,8 @@ const catalogue = [
   { name: "Leek", stock: 4, price: 0.9, category: "vegetable" },
 ];
 
-function reducer(state, action) {
-  switch (action.type) {
-    // Add a new object with the action.payload being the name of the product to the shopping cart state array
-    case "add_product":
-      return [...state, { productName: action.payload, quantity: 1 }];
-    case "increase_quantity": {
-      // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
-      const newState = state.map((product) => {
-        if (product.productName === action.payload) {
-          // Updates the quantity of the desired product
-          return {
-            productName: product.productName,
-            quantity: product.quantity + 1,
-          };
-          // For all items in the shopping cart array, dont do any changes
-        } else {
-          return product;
-        }
-      });
-      // return the new state of the shopping cart with quantity increased for a specific product
-      return newState;
-    }
-
-    default:
-      return state;
-  }
-}
-
 function ProductList() {
   const [state, dispatch] = useReducer(reducer, initialState);
-
-  // Adds a new object of a product to the shopping cart state
-  function handleAddToCart() {
-    dispatch({ type: "add_product", payload: "apple" });
-  }
-
-  // Increases quantity of existing products in the shopping cart state
-  function handleIncreaseQuantity(product) {
-    dispatch({ type: "increase_quantity", payload: product });
-  }
 
   return (
     <>
--- src/contexts/FoodContext.jsx
+++ src/contexts/FoodContext.jsx
@@ -1,9 +1,57 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useReducer } from "react";
 
 const FoodContext = createContext();
 
+  const initialState = [
+  { productName: "Leek", quantity: 2 },
+  { productName: "Broccoli", quantity: 5 },
+];
+
+
+function reducer(state, action) {
+  switch (action.type) {
+    // Add a new object with the action.payload being the name of the product to the shopping cart state array
+    case "add_product":
+      return [...state, { productName: action.payload, quantity: 1 }];
+    case "increase_quantity": {
+      // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
+      const newState = state.map((product) => {
+        if (product.productName === action.payload) {
+          // Updates the quantity of the desired product
+          return {
+            productName: product.productName,
+            quantity: product.quantity + 1,
+          };
+          // For all items in the shopping cart array, dont do any changes
+        } else {
+          return product;
+        }
+      });
+      // return the new state of the shopping cart with quantity increased for a specific product
+      return newState;
+    }
+
+    default:
+      return state;
+  }
+}
+
 function FoodProvider({ children }) {
-  return <FoodContext.Provider>{children}</FoodContext.Provider>;
+
+  // TODO: create constants below:
+  // const handleAddToCart
+  
+    // Adds a new object of a product to the shopping cart state
+  function handleAddToCart() {
+    dispatch({ type: "add_product", payload: "apple" });
+  }
+
+  // Increases quantity of existing products in the shopping cart state
+  function handleIncreaseQuantity(product) {
+    dispatch({ type: "increase_quantity", payload: product });
+  }
+
+  return <FoodContext.Provider value={}>{children}</FoodContext.Provider>
 }
 
 function useFood() {
