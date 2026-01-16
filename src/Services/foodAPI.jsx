2f97c82409e8b7b5017dcfa7f8b24f0c3a4430fc
BoSloth <leungjordan28@gmail.com>
2026-01-16 15:44:34 +0000

    Removed catalogue array from productList as being superceded by list of products from API

--- src/Services/foodAPI.jsx
+++ src/Services/foodAPI.jsx
@@ -1,7 +1,8 @@
 export async function fetchFood() {
-  const res = await fetch(
-    "https://api.nal.usda.gov/fdc/v1/foods/list?api_key=aKlhwHx8K8i2xffBqhMcopU3GDs3ksFb22hAQLnc&query=cheese"
-  );
-  const food = await res.json();
-  return food;
+    const res = await fetch(
+        "https://api.nal.usda.gov/fdc/v1/foods/list?api_key=aKlhwHx8K8i2xffBqhMcopU3GDs3ksFb22hAQLnc&query=cheese"
+    );
+    const food = await res.json();
+    console.log(food);
+    return food;
 }
--- src/components/ProductList.jsx
+++ src/components/ProductList.jsx
@@ -1,47 +1,20 @@
 import ProductItem from "./ProductItem.jsx";
 
-const catalogue = [
-  {
-    productName: "Leek",
-    stock: 8,
-    individualCost: 0.9,
-    category: "vegetable",
-  },
-  {
-    productName: "Broccoli",
-    stock: 15,
-    individualCost: 1.5,
-    category: "vegetable",
-  },
-  {
-    productName: "Golden Kiwi",
-    stock: 8,
-    individualCost: 2.15,
-    category: "fruit",
-  },
-  {
-    productName: "Croissant",
-    stock: 20,
-    individualCost: 0.5,
-    category: "bread",
-  },
-];
-
 function ProductList({ productList }) {
-  return (
-    <>
-      {productList?.map((food, index) => (
-        <ProductItem
-          name={food.description}
-          brand={food.brandOwner}
-          stock={food.stock}
-          // price={food.individualCost}
-          index={index}
-        ></ProductItem>
-      ))}
-      {/* <div>You have {state[0]?.quantity} apples in your cart!</div> */}
-    </>
-  );
+    return (
+        <>
+            {productList?.map((food, index) => (
+                <ProductItem
+                    name={food.description}
+                    brand={food.brandOwner}
+                    stock={food.stock}
+                    // price={food.individualCost}
+                    index={index}
+                ></ProductItem>
+            ))}
+            {/* <div>You have {state[0]?.quantity} apples in your cart!</div> */}
+        </>
+    );
 }
 
 export default ProductList;
