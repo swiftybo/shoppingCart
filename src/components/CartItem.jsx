9846977a712217c806c09617440d29cf5db6b2a5
Christian Yarwood <christianyarwood@gmail.com>
2026-01-17 17:29:03 +0000

    Cart removes item when the quantity reaches 0

--- src/components/CartItem.jsx
+++ src/components/CartItem.jsx
@@ -13,7 +13,7 @@ function CartItem({ item, name, quantity, individualCost }) {
     if (e.target.value < 0) {
       setQuantityInput(0);
     } else {
-      setQuantityInput(e.target.value);
+      setQuantityInput(Number(e.target.value));
     }
   }
 
@@ -26,7 +26,7 @@ function CartItem({ item, name, quantity, individualCost }) {
 
   // Update the input field when quantity changes (e.g. when the user presses the + or - minus buttons)
   useEffect(() => {
-    setQuantityInput(quantity);
+    setQuantityInput(Number(quantity));
   }, [quantity]);
 
   return (
@@ -46,7 +46,7 @@ function CartItem({ item, name, quantity, individualCost }) {
           className="text-normal cart-item-quantity"
           onKeyDown={handleUpdateQuantity}
           onChange={handleQuantity}
-          onBlur={(e) => setProductQuantity(name, e.target.value)}
+          onBlur={(e) => setProductQuantity(name, Number(e.target.value))}
           type="text"
           value={quantityInput}
           min="0"
--- src/contexts/FoodContext.jsx
+++ src/contexts/FoodContext.jsx
@@ -5,13 +5,13 @@ const FoodContext = createContext();
 const initialState = [];
 
 function reducer(state, action) {
+  const { description } = action.payload;
+  console.log(action.payload);
   switch (action.type) {
     // Add a new object with the action.payload being the name of the product to the shopping cart state array
     case "add_product":
-      return [
-        ...state,
-        { productName: action.payload.description, quantity: 1, price: 2 },
-      ];
+      return [...state, { productName: description, quantity: 1, price: 2 }];
+
     // Increase the cart quantity by using the button
     case "increase_quantity": {
       // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
@@ -29,29 +29,46 @@ function reducer(state, action) {
       });
       return newState;
     }
+
     // Decrease the cart quantity by using the button
     case "decrease": {
       // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
-      const newState = state.map((product) => {
-        if (product.productName === action.payload.productName) {
-          // Updates the quantity of the desired product by 1
-          if (product.quantity === 0) return { ...product };
-
-          return {
-            ...product,
-            quantity: product.quantity - 1,
-          };
-          // For all other items in the shopping cart array, dont do any changes
-        } else {
-          return product;
-        }
-      });
+      const newState = state
+        .map((product) => {
+          if (product.productName === action.payload.productName) {
+            // Updates the quantity of the desired product by 1
+            return {
+              ...product,
+              quantity: product.quantity - 1,
+            };
+            // For all other items in the shopping cart array, dont do any changes
+          } else {
+            return product;
+          }
+        })
+        // Removes the product from the cart when the quantity reaches 0
+        .filter((product) => product.quantity > 0);
       return newState;
     }
+
+    // This is for when the user specifically enters a quantity in the input element
     case "set_product_quantity": {
       // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
       const newState = state.map((product) => {
         if (product.productName === action.payload.name) {
+          if (action.payload.newQuantity <= 0) {
+            const newState = state.map((product) => {
+              if (product.productName === action.payload.productName) {
+                // If the product quantity reaches 0, then it removes it from the cart state
+                const filteredCart = state.cart.filter(
+                  (cartItem) =>
+                    cartItem.productname === action.payload.productname
+                );
+                console.log(filteredCart);
+              }
+            });
+            console.log(newState);
+          }
           // Updates the quantity of the desired product to the set amount
           return {
             ...product,
@@ -82,6 +99,12 @@ function FoodProvider({ children }) {
     dispatch({ type: "add_product", payload: product });
   }
 
+  // Removes the product from the state, for example when the user decrease the quantity to 0 in the cart
+  function handleRemoveFromCart(product) {
+    console.log("removed");
+    dispatch({ type: "remove_product", payload: product });
+  }
+
   // Increases quantity of existing products in the shopping cart state
   function handleIncreaseQuantity(product) {
     dispatch({ type: "increase_quantity", payload: product });
@@ -104,6 +127,7 @@ function FoodProvider({ children }) {
       value={{
         cart,
         handleAddToCart,
+        handleRemoveFromCart,
         handleIncreaseQuantity,
         setProductQuantity,
         handleDecreaseQuantity,
