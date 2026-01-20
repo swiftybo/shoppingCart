0282e7f058dd400af107226246fac646e50a4d57
BoSloth <leungjordan28@gmail.com>
2026-01-20 22:04:20 +0000

    added secondary condition to display 'last few in stock' message

--- src/components/ProductItem.jsx
+++ src/components/ProductItem.jsx
@@ -1,65 +1,67 @@
 import { useFood } from "../contexts/FoodContext";
 
 function ProductItem({ name, stock, product, price }) {
-  const { cart, handleAddToCart, handleIncreaseQuantity } = useFood();
+    const { cart, handleAddToCart, handleIncreaseQuantity } = useFood();
 
-  function addItem(product) {
-    console.log(product);
-    const itemAlreadyExists = cart.some(
-      (item) => item.productName === product.description
-    );
-    if (itemAlreadyExists) {
-      handleIncreaseQuantity(product);
-    } else {
-      handleAddToCart(product);
-      console.log(product);
+    function addItem(product) {
+        console.log(product);
+        const itemAlreadyExists = cart.some(
+            (item) => item.productName === product.description,
+        );
+        if (itemAlreadyExists) {
+            handleIncreaseQuantity(product);
+        } else {
+            handleAddToCart(product);
+            console.log(product);
+        }
     }
-  }
 
-  // This function takes the stock from state and calculates a derived state of
-  // the remaining stock using the original stock (saved in state) and subtracting
-  // from the amount in the cart. This results in it being impossible to add constraints
-  // on less than 0 stock being allowed.
-  function calculateRemainingStock() {
-    const identifiedItemIndex = cart.findIndex(
-      (item) => item.productName === name
+    // This function takes the stock from state and calculates a derived state of
+    // the remaining stock using the original stock (saved in state) and subtracting
+    // from the amount in the cart. This results in it being impossible to add constraints
+    // on less than 0 stock being allowed.
+    function calculateRemainingStock() {
+        const identifiedItemIndex = cart.findIndex(
+            (item) => item.productName === name,
+        );
+        let amountInCart = cart[identifiedItemIndex]
+            ? cart[identifiedItemIndex]?.quantity
+            : 0;
+        return stock - amountInCart;
+    }
+
+    const remainingStock = calculateRemainingStock();
+
+    return (
+        <section className="productIcon">
+            <div className="productIcon-left">
+                <h3>{name}</h3>
+                <p>£{price}.00</p>
+                <p className="color-grey">
+                    {remainingStock === 0
+                        ? "Out of stock"
+                        : `${remainingStock} in stock`}
+                </p>
+                {remainingStock <= 5 && remainingStock > 2 && (
+                    <div className="productIcon-stock">Almost Gone!</div>
+                )}
+                {remainingStock <= 2 && remainingStock > 0 && (
+                    <div className="productIcon-stock">Last Few in Stock!</div>
+                )}
+            </div>
+            <div className="productIcon-right">
+                <button
+                    className={`${
+                        remainingStock === 0 && "disabled-button"
+                    } productIcon_btn product-list-button`}
+                    onClick={() => addItem(product)}
+                    disabled={remainingStock === 0}
+                >
+                    {remainingStock === 0 ? "Out of Stock" : "Add to Cart"}
+                </button>
+            </div>
+        </section>
     );
-    let amountInCart = cart[identifiedItemIndex]
-      ? cart[identifiedItemIndex]?.quantity
-      : 0;
-    return stock - amountInCart;
-  }
-
-  const remainingStock = calculateRemainingStock();
-
-  return (
-    <section className="productIcon">
-      <div className="productIcon-left">
-        <h3>{name}</h3>
-        <p>£{price}.00</p>
-        <p className="color-grey">
-          {remainingStock === 0 ? "Out of stock" : `${remainingStock} in stock`}
-        </p>
-        {remainingStock <= 5 && remainingStock > 2 && (
-          <div className="productIcon-stock">Almost Gone!</div>
-        )}
-        {remainingStock <= 2 && (
-          <div className="productIcon-stock">Last Few in Stock!</div>
-        )}
-      </div>
-      <div className="productIcon-right">
-        <button
-          className={`${
-            remainingStock === 0 && "disabled-button"
-          } productIcon_btn product-list-button`}
-          onClick={() => addItem(product)}
-          disabled={remainingStock === 0}
-        >
-          {remainingStock === 0 ? "Out of Stock" : "Add to Cart"}
-        </button>
-      </div>
-    </section>
-  );
 }
 
 export default ProductItem;
--- src/contexts/FoodContext.jsx
+++ src/contexts/FoodContext.jsx
@@ -5,150 +5,160 @@ const FoodContext = createContext();
 const initialState = [];
 
 function reducer(state, action) {
-  const { description, individualCost } = action.payload;
-  console.log(action.payload);
-  switch (action.type) {
-    // Add a new object with the action.payload being the name of the product to the shopping cart state array
-    case "add_product":
-      return [
-        ...state,
-        { productName: description, quantity: 1, price: individualCost },
-      ];
+    const { description, individualCost } = action.payload;
+    console.log(action.payload);
+    switch (action.type) {
+        // Add a new object with the action.payload being the name of the product to the shopping cart state array
+        case "add_product":
+            return [
+                ...state,
+                {
+                    productName: description,
+                    quantity: 1,
+                    price: individualCost,
+                },
+            ];
 
-    // Increase the cart quantity by using the button
-    case "increase_quantity": {
-      // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
-      const newState = state.map((product) => {
-        if (product.productName === action.payload.productName) {
-          // Updates the quantity of the desired product by 1
-          return {
-            ...product,
-            quantity: product.quantity + 1,
-          };
-          // For all other items in the shopping cart array, dont do any changes
-        } else {
-          return product;
-        }
-      });
-      return newState;
-    }
-
-    // Decrease the cart quantity by using the button
-    case "decrease": {
-      // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
-      const newState = state
-        .map((product) => {
-          if (product.productName === action.payload.productName) {
-            // Updates the quantity of the desired product by 1
-            return {
-              ...product,
-              quantity: product.quantity - 1,
-            };
-            // For all other items in the shopping cart array, dont do any changes
-          } else {
-            return product;
-          }
-        })
-        // Removes the product from the cart when the quantity reaches 0
-        .filter((product) => product.quantity > 0);
-      return newState;
-    }
-
-    // This is for when the user specifically enters a quantity in the input element
-    case "set_product_quantity": {
-      // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
-      const newState = state.map((product) => {
-        if (product.productName === action.payload.name) {
-          if (action.payload.newQuantity <= 0) {
+        // Increase the cart quantity by using the button
+        case "increase_quantity": {
+            // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
             const newState = state.map((product) => {
-              if (product.productName === action.payload.productName) {
-                // If the product quantity reaches 0, then it removes it from the cart state
-                const filteredCart = state.cart.filter(
-                  (cartItem) =>
-                    cartItem.productname === action.payload.productname
-                );
-                console.log(filteredCart);
-              }
+                if (product.productName === action.payload.productName) {
+                    // Updates the quantity of the desired product by 1
+                    return {
+                        ...product,
+                        quantity: product.quantity + 1,
+                    };
+                    // For all other items in the shopping cart array, dont do any changes
+                } else {
+                    return product;
+                }
             });
-            console.log(newState);
-          }
-          // Updates the quantity of the desired product to the set amount
-          return {
-            ...product,
-            quantity: action.payload.newQuantity,
-          };
-          // For all other items in the shopping cart array, dont do any changes
-        } else {
-          return product;
+            return newState;
         }
-      });
-      // return the new state of the shopping cart with the new quantity for a specific product
-      return newState;
-    }
 
-    default:
-      return state;
-  }
+        // Decrease the cart quantity by using the button
+        case "decrease": {
+            // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
+            const newState = state
+                .map((product) => {
+                    if (product.productName === action.payload.productName) {
+                        // Updates the quantity of the desired product by 1
+                        return {
+                            ...product,
+                            quantity: product.quantity - 1,
+                        };
+                        // For all other items in the shopping cart array, dont do any changes
+                    } else {
+                        return product;
+                    }
+                })
+                // Removes the product from the cart when the quantity reaches 0
+                .filter((product) => product.quantity > 0);
+            return newState;
+        }
+
+        // This is for when the user specifically enters a quantity in the input element
+        case "set_product_quantity": {
+            // Iterates through the shopping cart state array to find a match for a specific product, e.g. apple
+            const newState = state.map((product) => {
+                if (product.productName === action.payload.name) {
+                    if (action.payload.newQuantity <= 0) {
+                        const newState = state.map((product) => {
+                            if (
+                                product.productName ===
+                                action.payload.productName
+                            ) {
+                                // If the product quantity reaches 0, then it removes it from the cart state
+                                const filteredCart = state.cart.filter(
+                                    (cartItem) =>
+                                        cartItem.productname ===
+                                        action.payload.productname,
+                                );
+                                console.log(filteredCart);
+                            }
+                        });
+                        console.log(newState);
+                    }
+                    // Updates the quantity of the desired product to the set amount
+                    return {
+                        ...product,
+                        quantity: action.payload.newQuantity,
+                    };
+                    // For all other items in the shopping cart array, dont do any changes
+                } else {
+                    return product;
+                }
+            });
+            // return the new state of the shopping cart with the new quantity for a specific product
+            return newState;
+        }
+
+        default:
+            return state;
+    }
 }
 
 function FoodProvider({ children }) {
-  const [cart, dispatch] = useReducer(reducer, initialState);
+    const [cart, dispatch] = useReducer(reducer, initialState);
 
-  // TODO: create constants below:
-  // const handleAddToCart
+    // TODO: create constants below:
+    // const handleAddToCart
 
-  // Adds a new object of a product to the shopping cart state
-  function handleAddToCart(product) {
-    dispatch({ type: "add_product", payload: product });
-  }
+    // Adds a new object of a product to the shopping cart state
+    function handleAddToCart(product) {
+        dispatch({ type: "add_product", payload: product });
+    }
 
-  // Removes the product from the state, for example when the user decrease the quantity to 0 in the cart
-  function handleRemoveFromCart(product) {
-    console.log("removed");
-    dispatch({ type: "remove_product", payload: product });
-  }
+    // Removes the product from the state, for example when the user decrease the quantity to 0 in the cart
+    function handleRemoveFromCart(product) {
+        console.log("removed");
+        dispatch({ type: "remove_product", payload: product });
+    }
 
-  // Increases quantity of existing products in the shopping cart state
-  function handleIncreaseQuantity(product) {
-    dispatch({ type: "increase_quantity", payload: product });
-  }
+    // Increases quantity of existing products in the shopping cart state
+    function handleIncreaseQuantity(product) {
+        dispatch({ type: "increase_quantity", payload: product });
+    }
 
-  // Decrease quantity of existing products in the shopping cart state
-  function handleDecreaseQuantity(product) {
-    dispatch({ type: "decrease", payload: product });
-  }
+    // Decrease quantity of existing products in the shopping cart state
+    function handleDecreaseQuantity(product) {
+        dispatch({ type: "decrease", payload: product });
+    }
 
-  function setProductQuantity(product, quantity) {
-    dispatch({
-      type: "set_product_quantity",
-      payload: { name: product, newQuantity: quantity },
-    });
-  }
+    function setProductQuantity(product, quantity) {
+        dispatch({
+            type: "set_product_quantity",
+            payload: { name: product, newQuantity: quantity },
+        });
+    }
 
-  return (
-    <FoodContext.Provider
-      value={{
-        cart,
-        handleAddToCart,
-        handleRemoveFromCart,
-        handleIncreaseQuantity,
-        setProductQuantity,
-        handleDecreaseQuantity,
-      }}
-    >
-      {children}
-    </FoodContext.Provider>
-  );
+    return (
+        <FoodContext.Provider
+            value={{
+                cart,
+                handleAddToCart,
+                handleRemoveFromCart,
+                handleIncreaseQuantity,
+                setProductQuantity,
+                handleDecreaseQuantity,
+            }}
+        >
+            {children}
+        </FoodContext.Provider>
+    );
 }
 
 function useFood() {
-  const context = useContext(FoodContext);
+    const context = useContext(FoodContext);
 
-  if (context === undefined) {
-    throw new Error("You used the FoodContext out of the FoodContext Provider");
-  }
+    if (context === undefined) {
+        throw new Error(
+            "You used the FoodContext out of the FoodContext Provider",
+        );
+    }
 
-  return context;
+    return context;
 }
 
 export { useFood, FoodProvider };
