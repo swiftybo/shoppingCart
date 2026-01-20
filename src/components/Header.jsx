f6b73c663d36245c97d7cfef0f67c6dcca6833d3
BoSloth <leungjordan28@gmail.com>
2026-01-20 21:50:05 +0000

    Added styling to header components to make the header more engaging

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,28 +3,28 @@ import { useFood } from "../contexts/FoodContext";
 import { LuShoppingCart } from "react-icons/lu";
 
 function Header() {
-  const { cart } = useFood();
+    const { cart } = useFood();
 
-  // Calculates the total quantity of all items in the cart to display in the cart button in the header
-  const totalCartQuantity = cart.reduce(
-    (acc, currentQuantity) => acc + currentQuantity.quantity,
-    0
-  );
+    // Calculates the total quantity of all items in the cart to display in the cart button in the header
+    const totalCartQuantity = cart.reduce(
+        (acc, currentQuantity) => acc + currentQuantity.quantity,
+        0,
+    );
 
-  return (
-    <div className="header">
-      <NavLink to={"/"} end>
-        <h1 className="header_name sixtyfour">Almighty's</h1>
-      </NavLink>
-      <div className="header-cart-container">
-        <Link to={"/cart"}>
-          <div className="product-list-button">
-            <LuShoppingCart /> Cart ({totalCartQuantity})
-          </div>
-        </Link>
-      </div>
-    </div>
-  );
+    return (
+        <div className="header">
+            <NavLink to={"/"} end>
+                <h1 className="header_name">CJ's</h1>
+            </NavLink>
+            <div className="header-cart-container">
+                <Link to={"/cart"}>
+                    <div className="product-list-button">
+                        <LuShoppingCart /> Cart ({totalCartQuantity})
+                    </div>
+                </Link>
+            </div>
+        </div>
+    );
 }
 
 export default Header;
